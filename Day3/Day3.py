b8b47ab1e7ca64b30eaa153434b1aad38d7cea78
Drake <louddrm@gmail.com>
2022-12-05 23:28:02 -0600

    Finished part 2

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,33 +1,50 @@
 import numpy as np
 
-def runPart1(compartments):
+def get_letter_val(letter):
+    if letter.isupper():
+        # 38 is magic number to convert uppercase letter to 27 through 52
+        return ord(letter) - 38
+    else:
+        # 96 is magic number to convert lowercase letter to 1 through 26
+        return ord(letter) - 96
+
+def runPart1(file_input):
     total = 0
 
-    for compartment in compartments:
+    for compartment in file_input:
         midway_i = len(compartment)//2
         compartment1 = compartment[:midway_i]
         compartment2 = compartment[midway_i:]
 
         for c in compartment1:
             if c in compartment2:
-                if c.isupper():
-                    # 38 is magic number to convert uppercase letter to 27 through 52
-                    total += ord(c) - 38
-                else:
-                    # 96 is magic number to convert lowercase letter to 1 through 26
-                    total += ord(c) - 96
+                total += get_letter_val(c)
                 break
     print(total)
 
-def runPart2(compartments):
-    pass
+def runPart2(file_input):
+    total = 0
+    for i in range(len(file_input)//3):
+        modifier = 3 * i
+        rucksack_1 = file_input[modifier + 0]
+        rucksack_2 = file_input[modifier + 1]
+        rucksack_3 = file_input[modifier + 2]
+
+        for c in rucksack_1:
+            if c in rucksack_2 and c in rucksack_3:
+                total += get_letter_val(c)
+                break
+
+    print(total)
+
+    
 
 if __name__ == '__main__':
     file = open('input.txt')
-    compartments = file.read().split('\n')
+    file_input = file.read().split('\n')
 
-    runPart1(compartments)
-    runPart2(compartments)
+    runPart1(file_input)
+    runPart2(file_input)
 
 
 
